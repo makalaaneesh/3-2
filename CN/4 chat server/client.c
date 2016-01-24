cefc2a18e8ef8dc6e29fbe1ac2d993c4407619ff
makalaaneesh <makalaaneesh@yahoo.com>
2016-01-24 23:25:46 +0530

    chat server 1.0

--- CN/4 chat server/client.c
+++ CN/4 chat server/client.c
@@ -10,10 +10,14 @@
 
 
 char *serverfifo = "server";
-char *buf = NULL;
-size_t bufsize;
+char myfifo[10];
+char *write_buf = NULL;
+char *read_buf = NULL;
+// char *msg;
+size_t write_bufsize;
 char *username;
-
+int client_to_server;
+int server_to_client;
 
 print_error(int val, char* msg){
 	if (val < 0){
@@ -22,24 +26,83 @@ print_error(int val, char* msg){
 	}
 }
 
-int main(){
 
-	// username = (char*)malloc(sizeof(char)*10);
-	// gets(username);
 
-	int fd = open(serverfifo, O_WRONLY);
-
+void* writing_thread(void * arg){
 	while(1){
-		ssize_t in  = getline(&buf, &bufsize, stdin);
-		print_error(in, "error reading!\n");
-		// printf("%s", buf);
+		ssize_t in  = getline(&write_buf, &write_bufsize, stdin);
+		print_error(in, "failed to get input");
+		// write_buf = (char*)malloc(sizeof(char)*PIPE_BUF);
+		// fgets(write_buf,PIPE_BUF,stdin);
 
-		int w = write(fd, buf, bufsize);
+		printf("%s", write_buf);
+
+		if(strcmp(write_buf, "exit") == 0){
+			printf("Quitting.\n");
+			exit(1);
+		}
+
+		char msg[PIPE_BUF];
+		snprintf(msg, sizeof(msg), "%s: %s",myfifo, write_buf);
+		int w = write(client_to_server, msg, sizeof(msg)); //writing to server
 		print_error(w, "write to fifo failed");
-		buf = NULL;
-
+		// printf("Wrote to server");
 
 	}
+	
+}
+
+void * reading_thread(void * arg){
+	while(1){
+
+		int r = read(server_to_client, read_buf, PIPE_BUF);
+		print_error(r, "read failed");
+		
+		printf("Server->%s", read_buf);
+		memset(read_buf, 0, sizeof(read_buf));
+
+	}
+	close(server_to_client);
+	unlink(myfifo);
+}
+
+void client_init(){	
+
+	// msg = (char *)malloc(sizeof(char)*100);
+
+
+	int pid = getpid();
+	sprintf(myfifo, "%d", pid);
+	// username = (char*)malloc(sizeof(char)*10);
+	// gets(username);
+	client_to_server = open(serverfifo, O_WRONLY);
+	printf("server fd is %d\n", client_to_server);
+	//init to server
+	
+	// opening fifo that the server will use to reply to the client
+	mkfifo(myfifo, 0666);
+	server_to_client = open(myfifo, O_RDWR);
+
+}
+
+
+
+int main(){
+	client_init();
+
+	char init_msg[PIPE_BUF];
+	snprintf(init_msg, sizeof(init_msg), "%s %s\n","init", myfifo);
+	printf("Intiial message is %s\n", init_msg);
+	int w = write(client_to_server, init_msg, sizeof(init_msg));
+	print_error(w, "initial write to fifo failed");
+
+
+	pthread_t wt, rt;
+	pthread_create(&wt, NULL, writing_thread, (void*)0);
+	// pthread_create(&rt, NULL, reading_thread, (void*)0);
+
+	pthread_join(wt,NULL);
+	// pthread_join(rt,NULL);
 	
 
 
--- CN/4 chat server/server.c
+++ CN/4 chat server/server.c
@@ -11,12 +11,12 @@
 char *fifo = "server";
 char *buf;
 
-char *client_fifos[MAX_CLIENTS];
+int client_fd_index;
 int client_fifos_fds[MAX_CLIENTS];
 
 print_error(int val, char* msg){
 	if (val < 0){
-		printf("%s\n", msg);
+		printf("%s error code = %d\n", msg, val);
 		exit(1);
 	}
 }
@@ -27,21 +27,52 @@ int server_init(){
 	mkfifo(fifo, 0666);
 	int fd = open(fifo, O_RDWR); //read write so that the fifo does not get closed when there are no clients
 	print_error(fd, "failed to open fifo");
+	client_fd_index = 0;
 	return fd;
 }
+
+
+void process_input(char *input){
+	char *stringp = (char *)malloc(sizeof(input));
+	strcpy(stringp, input);
+    const char *delim = " ";
+    char *token;
+ 
+    //stringp is updated to point to the next token 
+    token = strtok(stringp, delim); 
+    // printf("%s\n", token);
+    if(strcmp(token, "init") == 0 && stringp != NULL){
+    	printf("Adding new client\n");
+		token = strtok(NULL, delim);
+		char * client_pid = token;
+
+    	client_fifos_fds[client_fd_index++] = open(client_pid, O_WRONLY);
+    	printf("New client added %s", client_pid);
+    	return;
+    }	
+
+    printf("%s", input);
+	// int i;
+	// for(i = 0; i<client_fd_index; i++){
+	// 	int w = write(client_fifos_fds[i], buf, sizeof(buf)); //writing to each client
+	// 	print_error(w, "write to fifo failed");
+	// }
+	memset(input, 0, sizeof(input));
+}
+
 
 int main(){
 
 	int fd = server_init();
-	printf("Server ON.\n");
+	printf("Server ON.FD is %d\n", fd);
+
 
 	while(1){
 
 		int r = read(fd, buf, PIPE_BUF);
 		print_error(r, "read failed");
-		
-		printf("Server: %s", buf);
-		memset(buf, 0, sizeof(buf));
+		process_input(buf);
+
 
 	}
 	close(fd);
