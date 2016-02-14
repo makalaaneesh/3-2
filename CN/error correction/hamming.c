34a5af42b31d0a1d589eb7947fec175e2c9ba2f2
makalaaneesh <makalaaneesh@yahoo.com>
2016-02-14 23:30:24 +0530

    found r

--- CN/error correction/hamming.c
+++ CN/error correction/hamming.c
@@ -11,12 +11,27 @@ int check_bit_right(int val, int pos){// checks the bit if set to 1 or 0. pos st
 		return 0;
 }
 
-int check_bit_left(int val, int pos){// checks the bit if set to 1 or 0. pos starts from right.
+int check_bit_left(int val, int pos){// checks the bit if set to 1 or 0. pos starts from left.
 	pos = no_of_bits(val)- pos;
 	if ((val) & (1<<pos))
 		return 1;
 	else
 		return 0;
+}
+
+int set_bit(int *val, int pos){
+	// printf("setting %d",pos);
+	pos = no_of_bits((*val)) - pos; //accounting for starting indexing from 1.
+	printf("setting %d",pos);
+	*val = (*val) | (1<<pos);
+
+}
+int unset_bit(int *val, int pos){
+
+	pos = no_of_bits((*val)) - pos; //accounting for starting indexing from 1.
+	printf("unsetting %d",pos);
+	*val = (*val) & ~(1<<pos);
+	
 }
 
 int no_of_bits(int val){
@@ -44,12 +59,25 @@ void print_binary(int val){
 	}
 	printf("%s\n", "");
 }
+
+int r(int m){
+	int rval = no_of_bits(m);
+	// printf("initial rval is %d\n", rval);
+	// printf("log(m+r)%d is %d\n", (m+rval), no_of_bits(rval + m));
+	while(no_of_bits(rval + m) != rval )
+		rval++;
+	return rval;
+}
 int main(){
-	int a = 12314;
+	int a;
+	scanf("%d",&a);
 	print_binary(a);
-	int i;
-	for(i = 0; i<10; i++){
-		printf("power of 2 , %d is %d\n", i,power_2(i));
-	}
+	printf("%d\n", r(a));
+	// int i;
+
+
+	// for(i = 0; i<10; i++){
+	// 	printf("power of 2 , %d is %d\n", i,power_2(i));
+	// }
 	// printf("%d", check_bit(a,100));
 }
